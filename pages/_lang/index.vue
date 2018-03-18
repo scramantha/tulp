ace9d67b3f13c5601dbb73bc67d5e70e5ef9cc72
scramantha <samantha.mannino@gmail.com>
2018-03-18 20:08:47 +0100

    Set-up with nuxt starter-template-master + vue-i18n

---
+++ pages/_lang/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Content">
+    <div class="container">
+      <app-logo/>
+      <h1 class="Content__Title">{{ $t('home.title') }}</h1>
+      <p>{{ $t('home.introduction') }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  head() {
+    return { title: this.$t('home.title') }
+  },
+  components: {
+    AppLogo
+  }
+}
+</script>
